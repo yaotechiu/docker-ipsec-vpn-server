dbb5f199fc61ce93bf05407d21597b44bb67c655
yaotechiu <ted24821@gmail.com>
2020-07-30 01:58:25 +0800

    launch /etc/init.d/rsyslog at startup

--- run.sh
+++ run.sh
@@ -426,5 +426,6 @@ EOF
 mkdir -p /run/pluto /var/run/pluto /var/run/xl2tpd
 rm -f /run/pluto/pluto.pid /var/run/pluto/pluto.pid /var/run/xl2tpd.pid
 
+/etc/init.d/rsyslog start
 /usr/local/sbin/ipsec start
 exec /usr/sbin/xl2tpd -D -c /etc/xl2tpd/xl2tpd.conf
